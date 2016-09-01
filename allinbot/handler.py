318d50eb0b74172f2864a2f2124632d7d9fe77aa
Hugo Wainwright <wainwrighthugo@gmail.com>
2016-09-01 16:57:43 +0100

    Updated doc comments in handler.py

--- allinbot/handler.py
+++ allinbot/handler.py
@@ -6,7 +6,7 @@ class Handler:
     def can_handle_message(self, message: discord.Message) -> bool:
         """
         Messages will be passed to this function for the handler to decide whether it is interested in or not - this
-        function return true if and only if this is the case.
+        function should return true if and only if this is the case.
 
         Messages for which this function returned true will then be passed to the asynchronous handle_message()
         function.
@@ -16,8 +16,7 @@ class Handler:
     async def handle_message(self, client: discord.Client, message: discord.Message):
         """
         Messages for which can_handle_message() returned true will be passed to this handler for the handler to deal
-        with appropriately. The handler should then return an Awaitable bool representing whether the message was
-        handled by this handler or not.
+        with as appropriate.
         """
         raise NotImplementedError("handlers must implement this")
 
