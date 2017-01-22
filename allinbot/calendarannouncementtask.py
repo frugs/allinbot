d1760529568e0f93417db6c9f3a600f8ac5c8c91
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-01-22 19:51:48 +0000

    don't announce events within 5 days more than once in 24 hours announce 30 mins before event instead of 1 hour before

--- allinbot/calendarannouncementtask.py
+++ allinbot/calendarannouncementtask.py
@@ -12,15 +12,15 @@ _ANNOUNCEMENT_CHANNEL_ID = "195057719487627264"
 _ANNOUNCEMENT_TAG = "@here"
 _CALENDAR_URL = "https://calendar.google.com/calendar/ical/3om5b2vfubpugkf3vr6fahh01k%40group.calendar.google.com/public/basic.ics"
 _TIME_FORMAT = "%H:%M %d %b %Y %Z%z"
-_SECONDS_IN_HOUR = 3600
-_SECONDS_IN_DAY = _SECONDS_IN_HOUR * 24
+_FIVE_DAY_ANNOUNCMENT_INTERVAL = datetime.timedelta(days=1)
 
 
 class CalendarAnnouncementTask(Task):
 
     def __init__(self):
         self._five_minute_announced_events = {}
-        self._hour_announced_events = {}
+        self._thirty_minute_announced_events = {}
+        self._last_five_day_announcement = datetime.datetime.fromtimestamp(0, datetime.timezone.utc)
 
     async def perform_task(self, client: discord.Client):
         event_loop = client.loop
@@ -35,11 +35,11 @@ class CalendarAnnouncementTask(Task):
         now = datetime.datetime.now(datetime.timezone.utc)
 
         five_days_time = now + datetime.timedelta(days=5)
-        one_hours_time = now + datetime.timedelta(hours=1)
+        thirty_minutes_time = now + datetime.timedelta(minutes=30)
         five_minutes_time = now + datetime.timedelta(minutes=5)
 
         unannounced_events_in_next_five_minutes = []
-        unannounced_events_in_next_hour = []
+        unannounced_events_in_next_thirty_minutes = []
         events_in_next_5_days = []
 
         for component in calendar.walk():
@@ -60,17 +60,18 @@ class CalendarAnnouncementTask(Task):
                 if now < start_time <= five_minutes_time and component["UID"] not in self._five_minute_announced_events:
                     unannounced_events_in_next_five_minutes.append(event)
 
-                elif five_minutes_time < start_time <= one_hours_time and component["UID"] not in self._hour_announced_events:
-                    unannounced_events_in_next_hour.append(event)
+                elif five_minutes_time < start_time <= thirty_minutes_time and component["UID"] not in self._thirty_minute_announced_events:
+                    unannounced_events_in_next_thirty_minutes.append(event)
 
-                elif one_hours_time < start_time <= five_days_time:
+                elif thirty_minutes_time < start_time <= five_days_time:
                     events_in_next_5_days.append(event)
 
-        # only make an announcement if we have an unannounced event coming up in the next hour or in the next 5 mins
-        if unannounced_events_in_next_five_minutes or unannounced_events_in_next_hour:
+        # only make an announcement if we have an unannounced event coming up in the next thirty minutes or in the
+        # next 5 mins
+        if unannounced_events_in_next_five_minutes or unannounced_events_in_next_thirty_minutes:
 
             unannounced_events_in_next_five_minutes.sort(key=lambda x:x[0])
-            unannounced_events_in_next_hour.sort(key=lambda x: x[0])
+            unannounced_events_in_next_thirty_minutes.sort(key=lambda x: x[0])
             events_in_next_5_days.sort(key=lambda x: x[0])
 
             messages_to_send = [_ANNOUNCEMENT_TAG]
@@ -87,20 +88,22 @@ class CalendarAnnouncementTask(Task):
 
                 messages_to_send.append(five_minute_reminder_message)
 
-            if unannounced_events_in_next_hour:
-                hour_reminder_message = "__Events starting in the next hour:__\n"
+            if unannounced_events_in_next_thirty_minutes:
+                thirty_minute_reminder_message = "__Events starting in the next thirty minutes:__\n"
 
-                for start_time, event in unannounced_events_in_next_hour:
+                for start_time, event in unannounced_events_in_next_thirty_minutes:
                     start_time_string = calendar_timezone.normalize(start_time).strftime(_TIME_FORMAT)
                     time_delta_string = calendarutils.describe_time_delta(start_time - now, minutes=True)
 
-                    hour_reminder_message += "In {} - **{}**\n*{}*\n".format(
+                    thirty_minute_reminder_message += "In {} - **{}**\n*{}*\n".format(
                         time_delta_string, event["SUMMARY"], start_time_string)
-                    self._hour_announced_events[event["UID"]] = start_time
+                    self._thirty_minute_announced_events[event["UID"]] = start_time
 
-                messages_to_send.append(hour_reminder_message)
+                messages_to_send.append(thirty_minute_reminder_message)
 
-            if events_in_next_5_days:
+            if events_in_next_5_days and now - self._last_five_day_announcement > _FIVE_DAY_ANNOUNCMENT_INTERVAL:
+                self._last_five_day_announcement = now
+                
                 announcement_message = "__Upcoming events in the next 5 days:__\n"
 
                 for start_time, event in events_in_next_5_days:
@@ -116,4 +119,4 @@ class CalendarAnnouncementTask(Task):
             await client.send_message(channel, "\n".join(messages_to_send))
 
             self._five_minute_announced_events = dict(filter(lambda x: x[1] >= now, self._five_minute_announced_events.items()))
-            self._hour_announced_events = dict(filter(lambda x: x[1] >= now, self._hour_announced_events.items()))
+            self._thirty_minute_announced_events = dict(filter(lambda x: x[1] >= now, self._thirty_minute_announced_events.items()))
