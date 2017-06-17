f0fe8a9a07df734464385bdce33fbe8ddabf90cc
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-06-17 19:40:03 +0100

    add inject efficiency from first queen inject

--- allinbot/replay/injectefficiency.py
+++ allinbot/replay/injectefficiency.py
@@ -19,13 +19,21 @@ def _convert_inject_states_to_data_set(inject_states: list, fps: int) -> list:
 
 def generate_inject_efficiency_page_data_for_player(player: Player, replay: Replay) -> str:
     inject_states = techlabreactor.get_hatchery_inject_states_for_player(player, replay)
+
     chart_data = _convert_inject_states_to_data_set(inject_states, int(replay.game_fps))
-    inject_efficiency = techlabreactor.calculate_inject_efficiency(inject_states)
-    inject_efficiency_str = "{0:.2f}".format(inject_efficiency * 100)
+
+    overall_inject_efficiency = techlabreactor.calculate_overall_inject_efficiency(inject_states)
+    overall_inject_efficiency_str = "{0:.2f}".format(overall_inject_efficiency * 100)
+
+    inject_efficiency_from_first_queen_completed = techlabreactor.calculate_inject_efficiency_from_frame(
+        techlabreactor.find_first_queen_completed_frame_for_player(player, replay),
+        inject_states)
+    inject_efficiency_from_first_queen_completed_str = "{0:.2f}".format(inject_efficiency_from_first_queen_completed * 100)
 
     json_string = json.dumps({
         "chartData": chart_data,
-        "injectEfficiency": inject_efficiency_str,
+        "overallInjectEfficiency": overall_inject_efficiency_str,
+        "injectEfficiencyFromFirstQueenCompleted": inject_efficiency_from_first_queen_completed_str,
         "playerName": player.name
     })
     return urllib.parse.quote(json_string)
