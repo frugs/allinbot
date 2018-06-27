161ae52788cf173456c0029d95b120e4da16cc96
Hugo Wainwright <hugowainwright@fb.com>
2018-06-27 14:17:23 +0100

    racementionhandler - remove use of deprecated 'discord_id' key, name offline members

--- allinbot/racementionhandlers.py
+++ allinbot/racementionhandlers.py
@@ -7,8 +7,6 @@ import pyrebase
 from .database import perform_database_task, DatabaseTask
 from .handler import Handler
 
-ALLIN_MEMBER_ROLE_ID = os.getenv("ALLIN_MEMBER_ROLE_ID")
-
 
 class QueryRacePlayerDiscordIdsDatabaseTask(DatabaseTask[typing.List[str]]):
     def __init__(self, race: str, db_config: dict):
@@ -24,8 +22,8 @@ class QueryRacePlayerDiscordIdsDatabaseTask(DatabaseTask[typing.List[str]]):
             return []
         else:
             return [
-                member["discord_id"] for member in query_result.val().values()
-                if 'discord_id' in member
+                member.key() for member in query_result.each()
+                if member.val().get("is_full_member", False)
             ]
 
 
@@ -33,7 +31,7 @@ class RaceMentionHandler(Handler):
     def __init__(self, race: str, db_config: dict):
         self._race = race
         self._db_config = db_config
-        self._matcher = re.compile("^@{}\s+(.*)$".format(race.lower()),
+        self._matcher = re.compile("^@{}(?:\s+)?(.*)$".format(race.lower()),
                                    re.IGNORECASE)
 
     async def handle_message(self, client: discord.Client,
@@ -41,24 +39,30 @@ class RaceMentionHandler(Handler):
         ids = await perform_database_task(
             client.loop,
             QueryRacePlayerDiscordIdsDatabaseTask(self._race, self._db_config))
-        server_members = filter(
-            None,
-            [message.server.get_member(discord_id) for discord_id in ids])
-        online_server_members = [
-            member for member in server_members
-            if any(role for role in member.roles
-                   if role.id == ALLIN_MEMBER_ROLE_ID) and (
-                       member.status == discord.Status.online
-                       or member.status == discord.Status.idle)
-        ]
-        mentions = ", ".join(
-            [member.mention for member in online_server_members])
+
+        def mention_if_online_and_idle(discord_id: str) -> str:
+            discord_member = message.server.get_member(discord_id)
+            if not discord_member:
+                return ""
+
+            if discord_member.status in [
+                    discord.Status.online, discord.Status.idle
+            ]:
+                return "<@{}>".format(discord_id)
+            else:
+                nick = discord_member.nick
+                return nick if nick else discord_member.name
+
+        mentions_and_names = list(
+            filter(lambda x: x, map(mention_if_online_and_idle, ids)))
+
+        mentions_and_names_str = ", ".join(mentions_and_names)
 
         match = re.match(self._matcher, message.content)
         message_content = match.group(1) if match else ""
 
-        await client.send_message(message.channel,
-                                  message_content + "\n" + mentions)
+        await client.send_message(
+            message.channel, message_content + "\n" + mentions_and_names_str)
 
     def can_handle_message(self, message: discord.Message) -> bool:
         return message.server and (message.content.casefold() == "@{}".format(
@@ -66,8 +70,9 @@ class RaceMentionHandler(Handler):
                                    or re.match(self._matcher, message.content))
 
     async def description(self, client) -> str:
-        return "@" + self._race.lower(
-        ) + " *{message}* - @mention members who play " + self._race + " with {message}"
+        return ("@{} *{{message}}* -"
+                "@mention members who play {} with {{message}}").format(
+                    self._race.lower(), self._race)
 
 
 def zerg_mention_handler(db_config: dict) -> Handler:
